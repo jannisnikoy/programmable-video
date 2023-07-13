5a7b0ca2d196cfcb2aa75ca4b309d19f837ce1e8
Jannis Nikou <jannisnikoy@me.com>
2023-07-13 13:47:35 -0700

    version bump

--- programmable_video_web/lib/src/interop/version.dart
+++ programmable_video_web/lib/src/interop/version.dart
@@ -4,7 +4,7 @@ library interop;
 import 'package:js/js.dart';
 import 'package:version/version.dart';
 
-final supportedVersion = Version(2, 14, 0);
+final supportedVersion = Version(2, 27, 0);
 
 @JS('Twilio.Video.version')
 external String version;
